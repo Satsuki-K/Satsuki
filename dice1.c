5a0cfabe8311dea1f4a18c30755dd95e4d8a56dd
Kawahara Satsuki <may@wlan-napt-001.u-aizu.ac.jp>
2018-07-09 16:21:27 +0900

    dice1.c

---
+++ dice1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+  int a[2],sum,i;
+
+  sum = 0;
+
+  srand((unsigned) time(NULL));
+  
+  printf("Rolling the dice...\n");
+  for(i=0;i<2;i++){
+    printf("Dice %d: ",i+1);
+    a[i] = rand()%6+1;
+    sum += a[i];
+    printf("%d\n",a[i]);
+  }
+
+  printf("Total value: %d\n",sum);
+
+  if(sum>=7) printf("You won!\n");
+  else  printf("You lost!\n");
+
+  return 0;
+}
